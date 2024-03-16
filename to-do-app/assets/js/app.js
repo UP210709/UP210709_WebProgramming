9f53beb01c2822516c115225fe2e7250570e337e
Aarón Quiñones Mtz <113202726+UP210709@users.noreply.github.com>
2024-03-15 20:56:46 -0600

    Show tasks

--- to-do-app/assets/js/app.js
+++ to-do-app/assets/js/app.js
@@ -2,40 +2,66 @@
 const userSelect = document.getElementById('select-users');
 const userContainer = document.getElementById('user-container');
 const taskContainer = document.getElementById('task-container');
+const showTasksButton=document.getElementById('show-tasks-button');
 
 // Codígo nesesario para mostrar información
+const userTasks=taskContainer.children[1];
 const userInformation=userContainer.children[1];
 let usersList=[];
 
-
+userTasks.innerText="";
 userSelect.innerText="";
 
 getAllUsers()
 .then((response)=>{
-  
   for(let i=0;i<response.length;i++){
     userSelect.innerHTML+="<option value="+i+">"+response[i].firstname+"</option>";
     usersList[i]=response[i];
   }
 
-  showUserInformation(response[0].firstname,response[0].lastname,response[0].email);
-})
+  userInformation.innerText="";
+
+  userInformation.innerHTML+="<li>ID: "+response[0].id+"</li>";
+  userInformation.innerHTML+="<li>Nombre completo: "+response[0].firstname+" "+response[0].lastname+"</li>";
+  userInformation.innerHTML+="<li>Email: "+response[0].email+"</li>";
+});
+
 
 userSelect.addEventListener("change",()=>{
-  const userNumber=userSelect.value
-  showUserInformation(usersList[userNumber].firstname,usersList[userNumber].lastname,usersList[userNumber].email);
-})
+  const userNumber=userSelect.value;
+  userInformation.innerText="";
+  userTasks.innerText="";
+
+  userInformation.innerHTML+="<li>ID: "+usersList[userNumber].id+"</li>";
+  userInformation.innerHTML+="<li>Nombre completo: "+usersList[userNumber].firstname+" "+usersList[userNumber].lastname+"</li>";
+  userInformation.innerHTML+="<li>Email: "+usersList[userNumber].email+"</li>";
+
+});
+
+showTasksButton.addEventListener("click",()=>{
+  if(userTasks.innerText==""){
+    userTasks.innerText="";
+    const user_ID=usersList[userSelect.value].id;
+    let checkboxState="";
+
+    getAllTasks()
+    .then((response)=>{
+      for(let i=0;i<response.length;i++){
+        if(response[i].userId===user_ID){
+          checkboxState="";
+          if(response[i].completed)
+            checkboxState="checked";
+
+          userTasks.innerHTML+="<li><input type='checkbox'"+checkboxState+"><span>"+response[i].title+"</span></li>";
+          
+        }
+      }
+    });
+  }
+});
 // Fin de codígo 
 
 // Funciones
-
-function showUserInformation(firstname,lastname,email){
-  userInformation.innerText="";
-
-  userInformation.innerHTML+="<li>Nombre completo: "+firstname+" "+lastname+"</li>";
-  userInformation.innerHTML+="<li>Email: "+email+"</li>";
-}
-
 
 /**
  * Optiene una lista de todos los usuarios que pueden existir
@@ -51,7 +77,7 @@ function getAllUsers() {
  * @returns {Promise<Task[]>}
  */
 function getAllTasks() {
-  return fetch('/data/usuarios.json')
+  return fetch('/data/tareas.json')
     .then(resp => resp.json());
 }
 
